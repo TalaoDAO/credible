1c338426b25a2685f50b7ee27926cab0396f8ea8
hawkbee1 <googandads@gmail.com>
2021-07-13 11:51:45 +0200

    Add Talao design

--- lib/app/shared/ui/ui.dart
+++ lib/app/shared/ui/ui.dart
@@ -1,3 +1,7 @@
+import 'package:credible/app/shared/ui/talao/constraints.dart';
+import 'package:credible/app/shared/ui/talao/palette.dart';
+import 'package:credible/app/shared/ui/talao/text.dart';
+
 import 'base/constraints.dart';
 import 'base/palette.dart';
 import 'base/text.dart';
@@ -8,7 +12,7 @@ import 'degen/constraints.dart';
 import 'degen/palette.dart';
 import 'degen/text.dart';
 
-const UiKit = Ui._degen();
+const UiKit = Ui._talao();
 
 class Ui {
   final UiPalette palette;
@@ -27,5 +31,12 @@ class Ui {
     this.palette = const DegenPalette(),
     this.constraints = const DegenConstraints(),
     this.text = const DegenText(),
+  });
+
+  // ignore: unused_element
+  const Ui._talao({
+    this.palette = const TalaoPalette(),
+    this.constraints = const TalaoConstraints(),
+    this.text = const TalaoText(),
   });
 }
--- lib/app/shared/widget/brand.dart
+++ lib/app/shared/widget/brand.dart
@@ -1,7 +1,6 @@
 import 'package:credible/app/shared/widget/hero_workaround.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class BrandMinimal extends StatelessWidget {
   @override
@@ -15,7 +14,7 @@ class BrandMinimal extends StatelessWidget {
           child: Container(
             width: MediaQuery.of(context).size.width * 0.2,
             height: MediaQuery.of(context).size.width * 0.2,
-            child: SvgPicture.asset('assets/brand/logo-splash.svg'),
+            child: Image.asset('assets/brand/logo_talao.png'),
           ),
         ),
       ],
@@ -34,7 +33,7 @@ class Brand extends StatelessWidget {
             child: Container(
               width: MediaQuery.of(context).size.width * 0.2,
               height: MediaQuery.of(context).size.width * 0.2,
-              child: SvgPicture.asset('assets/brand/logo-circular.svg'),
+              child: Image.asset('assets/brand/logo_talao.png'),
             ),
           ),
           const SizedBox(height: 32.0),
@@ -44,7 +43,7 @@ class Brand extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                SvgPicture.asset('assets/brand/logo-title.svg'),
+                Image.asset('assets/brand/logo_talao.png'),
                 // const SizedBox(height: 16.0),
                 // SvgPicture.asset('assets/brand/logo-subtitle.svg'),
               ],
