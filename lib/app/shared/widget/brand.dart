1704593cb7e312677930c1599a4812f83fb9281c
hawkbee1 <googandads@gmail.com>
2021-09-21 11:19:59 +0200

    new proposition for landing page design #32

--- lib/app/shared/widget/brand.dart
+++ lib/app/shared/widget/brand.dart
@@ -26,18 +26,13 @@ class Brand extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Column(
         mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const SizedBox(height: 32.0),
           HeroFix(
             tag: 'splash/title',
             child: Column(
               mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Image.asset('assets/brand/logo_talao.png'),
-                // const SizedBox(height: 16.0),
-                // SvgPicture.asset('assets/brand/logo-subtitle.svg'),
               ],
             ),
           ),
