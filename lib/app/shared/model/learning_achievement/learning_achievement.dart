95317cb5733de79050dad721c2daf62cc6021aa3
hawkbee1 <googandads@gmail.com>
2021-09-22 12:20:31 +0200

    Nouveau VC LearningAchievement #37

---
+++ lib/app/shared/model/learning_achievement/learning_achievement.dart
@@ -0,0 +1,72 @@
+import 'package:talao/app/pages/credentials/models/credential.dart';
+import 'package:talao/app/pages/credentials/widget/display_issuer.dart';
+import 'package:talao/app/shared/model/author.dart';
+import 'package:talao/app/shared/model/credential_subject.dart';
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:talao/app/shared/model/learning_achievement/has_credential.dart';
+import 'package:talao/app/shared/widget/base/credential_field.dart';
+
+part 'learning_achievement.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class LearningAchievement extends CredentialSubject {
+  @override
+  String id;
+  @override
+  String type;
+  @JsonKey(defaultValue: '')
+  String familyName;
+  @JsonKey(defaultValue: '')
+  String givenName;
+  @JsonKey(defaultValue: '')
+  String email;
+  @JsonKey(defaultValue: '')
+  String birthDate;
+  HasCredential hasCredential;
+  @override
+  final Author issuedBy;
+
+  LearningAchievement(this.id, this.type, this.familyName, this.givenName,
+      this.email, this.birthDate, this.hasCredential, this.issuedBy)
+      : super(id, type, issuedBy);
+
+  factory LearningAchievement.fromJson(Map<String, dynamic> json) =>
+      _$LearningAchievementFromJson(json);
+
+  @override
+  Map<String, dynamic> toJson() => _$LearningAchievementToJson(this);
+
+  @override
+  Widget displayInList(BuildContext context, CredentialModel item) {
+    return Text('display list certificate');
+  }
+
+  @override
+  Widget displayDetail(BuildContext context, CredentialModel item) {
+    final localizations = AppLocalizations.of(context)!;
+
+    return Column(
+      children: [
+        CredentialField(value: familyName, title: localizations.firstName),
+        CredentialField(title: localizations.lastName, value: givenName),
+        CredentialField(title: localizations.personalMail, value: email),
+        CredentialField(title: localizations.birthdate, value: birthDate),
+        CredentialField(
+            title: localizations.learningAchievement,
+            value: hasCredential.title),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(hasCredential.description),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: DisplayIssuer(
+            issuer: issuedBy,
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/app/shared/model/learning_achievement/learning_achievement.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'learning_achievement.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LearningAchievement _$LearningAchievementFromJson(Map<String, dynamic> json) {
+  return LearningAchievement(
+    json['id'] as String,
+    json['type'] as String,
+    json['familyName'] as String? ?? '',
+    json['givenName'] as String? ?? '',
+    json['email'] as String? ?? '',
+    json['birthDate'] as String? ?? '',
+    HasCredential.fromJson(json['hasCredential'] as Map<String, dynamic>),
+    CredentialSubject.fromJsonAuthor(json['issuedBy']),
+  );
+}
+
+Map<String, dynamic> _$LearningAchievementToJson(
+        LearningAchievement instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'type': instance.type,
+      'familyName': instance.familyName,
+      'givenName': instance.givenName,
+      'email': instance.email,
+      'birthDate': instance.birthDate,
+      'hasCredential': instance.hasCredential,
+      'issuedBy': instance.issuedBy,
+    };
