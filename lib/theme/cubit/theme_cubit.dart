77dee3df9dc07e566bfe868b1b4e3addbec5484e
Bibash Shrestha <bibashshrestha@gmail.com>
2022-02-01 16:57:19 +0545

    set system theme as default

--- lib/theme/cubit/theme_cubit.dart
+++ lib/theme/cubit/theme_cubit.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:talao/app/interop/secure_storage/secure_storage.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
-  ThemeCubit() : super(ThemeMode.dark);
+  ThemeCubit() : super(ThemeMode.system);
 
   Future<void> setLightTheme() async {
     await SecureStorageProvider.instance.set('theme', 'light');
