5d478a35b95219b5b5890985b9a7c728276dbeff
Bibash Shrestha <bibashshrestha@gmail.com>
2022-02-16 12:03:45 +0545

    adopted to drawer property

--- lib/profile/view/profile_page.dart
+++ lib/profile/view/profile_page.dart
@@ -30,122 +30,119 @@ class ProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
 
-    return BlocConsumer(
-      bloc: context.read<ProfileBloc>(),
-      listener: (context, state) {},
-      builder: (context, state) {
-        final model =
-            state is ProfileStateDefault ? state.model : ProfileModel();
-        final firstName = model.firstName;
-        final lastName = model.lastName;
+    return Drawer(
+      child: SafeArea(
+        child: BlocConsumer(
+          bloc: context.read<ProfileBloc>(),
+          listener: (context, state) {},
+          builder: (context, state) {
+            final model =
+                state is ProfileStateDefault ? state.model : ProfileModel();
+            final firstName = model.firstName;
+            final lastName = model.lastName;
 
-        return BasePage(
-          title: l10n.profileTitle,
-          padding: const EdgeInsets.symmetric(
-            vertical: 24.0,
-          ),
-          navigation: CustomNavBar(index: 2),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              if (firstName.isNotEmpty || lastName.isNotEmpty)
-                Center(
-                  child: Text(
-                    '$firstName $lastName',
-                    style: Theme.of(context).textTheme.subtitle1!,
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                if (firstName.isNotEmpty || lastName.isNotEmpty)
+                  Center(
+                    child: Text(
+                      '$firstName $lastName',
+                      style: Theme.of(context).textTheme.subtitle1!,
+                    ),
                   ),
+                const SizedBox(height: 32.0),
+                MenuItem(
+                  icon: Icons.person,
+                  title: l10n.personalTitle,
+                  onTap: () =>
+                      Navigator.of(context).push(PersonalPage.route(model)),
                 ),
-              const SizedBox(height: 32.0),
-              MenuItem(
-                icon: Icons.person,
-                title: l10n.personalTitle,
-                onTap: () =>
-                    Navigator.of(context).push(PersonalPage.route(model)),
-              ),
-              MenuItem(
-                icon: Icons.receipt_long,
-                title: l10n.globalInformationLabel,
-                onTap: () =>
-                    Navigator.of(context).push(GlobalInformationPage.route()),
-              ),
-              MenuItem(
-                icon: Icons.shield,
-                title: l10n.privacyTitle,
-                onTap: () => Navigator.of(context).push(PrivacyPage.route()),
-              ),
-              MenuItem(
-                icon: Icons.article,
-                title: l10n.onBoardingTosTitle,
-                onTap: () => Navigator.of(context).push(TermsPage.route()),
-              ),
-              MenuItem(
-                icon: Icons.vpn_key,
-                title: l10n.recoveryTitle,
-                onTap: () async {
-                  final confirm = await showDialog<bool>(
-                        context: context,
-                        builder: (context) => ConfirmDialog(
-                          title: l10n.recoveryWarningDialogTitle,
-                          subtitle: l10n.recoveryWarningDialogSubtitle,
-                          yes: l10n.showDialogYes,
-                          no: l10n.showDialogNo,
-                        ),
-                      ) ??
-                      false;
+                MenuItem(
+                  icon: Icons.receipt_long,
+                  title: l10n.globalInformationLabel,
+                  onTap: () =>
+                      Navigator.of(context).push(GlobalInformationPage.route()),
+                ),
+                MenuItem(
+                  icon: Icons.shield,
+                  title: l10n.privacyTitle,
+                  onTap: () => Navigator.of(context).push(PrivacyPage.route()),
+                ),
+                MenuItem(
+                  icon: Icons.article,
+                  title: l10n.onBoardingTosTitle,
+                  onTap: () => Navigator.of(context).push(TermsPage.route()),
+                ),
+                MenuItem(
+                  icon: Icons.vpn_key,
+                  title: l10n.recoveryTitle,
+                  onTap: () async {
+                    final confirm = await showDialog<bool>(
+                          context: context,
+                          builder: (context) => ConfirmDialog(
+                            title: l10n.recoveryWarningDialogTitle,
+                            subtitle: l10n.recoveryWarningDialogSubtitle,
+                            yes: l10n.showDialogYes,
+                            no: l10n.showDialogNo,
+                          ),
+                        ) ??
+                        false;
 
-                  if (confirm) {
-                    await Navigator.of(context).push(RecoveryPage.route());
-                  }
-                },
-              ),
-              MenuItem(
-                icon: Icons.settings_backup_restore,
-                title: l10n.resetWalletButton,
-                onTap: () async {
-                  final confirm = await showDialog<bool>(
-                        context: context,
-                        builder: (context) => ConfirmDialog(
-                          title: l10n.resetWalletButton,
-                          subtitle: l10n.resetWalletConfirmationText,
-                          yes: l10n.showDialogYes,
-                          no: l10n.showDialogNo,
-                        ),
-                      ) ??
-                      false;
+                    if (confirm) {
+                      await Navigator.of(context).push(RecoveryPage.route());
+                    }
+                  },
+                ),
+                MenuItem(
+                  icon: Icons.settings_backup_restore,
+                  title: l10n.resetWalletButton,
+                  onTap: () async {
+                    final confirm = await showDialog<bool>(
+                          context: context,
+                          builder: (context) => ConfirmDialog(
+                            title: l10n.resetWalletButton,
+                            subtitle: l10n.resetWalletConfirmationText,
+                            yes: l10n.showDialogYes,
+                            no: l10n.showDialogNo,
+                          ),
+                        ) ??
+                        false;
 
-                  if (confirm) {
-                    await SecureStorageProvider.instance.delete('key');
-                    await SecureStorageProvider.instance.delete('mnemonic');
-                    await SecureStorageProvider.instance.delete('data');
+                    if (confirm) {
+                      await SecureStorageProvider.instance.delete('key');
+                      await SecureStorageProvider.instance.delete('mnemonic');
+                      await SecureStorageProvider.instance.delete('data');
 
-                    await SecureStorageProvider.instance
-                        .delete(ProfileModel.firstNameKey);
-                    await SecureStorageProvider.instance
-                        .delete(ProfileModel.lastNameKey);
-                    await SecureStorageProvider.instance
-                        .delete(ProfileModel.phoneKey);
-                    await SecureStorageProvider.instance
-                        .delete(ProfileModel.locationKey);
-                    await SecureStorageProvider.instance
-                        .delete(ProfileModel.emailKey);
+                      await SecureStorageProvider.instance
+                          .delete(ProfileModel.firstNameKey);
+                      await SecureStorageProvider.instance
+                          .delete(ProfileModel.lastNameKey);
+                      await SecureStorageProvider.instance
+                          .delete(ProfileModel.phoneKey);
+                      await SecureStorageProvider.instance
+                          .delete(ProfileModel.locationKey);
+                      await SecureStorageProvider.instance
+                          .delete(ProfileModel.emailKey);
 
-                    await context.read<WalletBloc>().deleteAll();
+                      await context.read<WalletBloc>().deleteAll();
 
-                    await Navigator.of(context).push(SplashPage.route());
-                  }
-                },
-              ),
-              MenuItem(
-                key: Key('theme_update'),
-                icon: Icons.light_mode,
-                title: l10n.selectThemeText,
-                onTap: () => Navigator.of(context)
-                    .push(ThemePage.route(context.read<ThemeCubit>())),
-              ),
-            ],
-          ),
-        );
-      },
+                      await Navigator.of(context).push(SplashPage.route());
+                    }
+                  },
+                ),
+                MenuItem(
+                  key: Key('theme_update'),
+                  icon: Icons.light_mode,
+                  title: l10n.selectThemeText,
+                  onTap: () => Navigator.of(context)
+                      .push(ThemePage.route(context.read<ThemeCubit>())),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
