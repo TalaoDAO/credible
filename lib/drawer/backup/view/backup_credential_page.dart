e9b5b43a805ed763b8eac7d8b84397e754902699
Bibash Shrestha <bibashshrestha@gmail.com>
2022-02-24 08:53:52 +0545

    attempting to write file

--- lib/drawer/backup/view/backup_credential_page.dart
+++ lib/drawer/backup/view/backup_credential_page.dart
@@ -1,24 +1,39 @@
 import 'dart:io';
+import 'package:downloads_path_provider_28/downloads_path_provider_28.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
-
+import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:talao/app/shared/widget/app_version.dart';
 import 'package:talao/app/shared/widget/back_leading_button.dart';
 import 'package:talao/app/shared/widget/base/button.dart';
 import 'package:talao/app/shared/widget/base/page.dart';
-import 'package:talao/drawer/global_information/bloc/did_bloc.dart';
-import 'package:talao/drawer/global_information/view/did_display.dart';
-import 'package:talao/drawer/global_information/view/display_application_contacts.dart';
-import 'package:talao/drawer/global_information/view/issuer_verification_setting.dart';
 import 'package:talao/l10n/l10n.dart';
+import 'package:path/path.dart' as path;
 
 class BackupCredentialPage extends StatelessWidget {
   static Route route() => MaterialPageRoute(
         builder: (_) => BackupCredentialPage(),
         settings: RouteSettings(name: '/backupCredentialPage'),
       );
+
+  Future<Directory?> _getDownloadDirectory() async {
+    if (Platform.isAndroid) {
+      return await DownloadsPathProvider.downloadsDirectory;
+    }
+    return await getApplicationDocumentsDirectory();
+  }
+
+  Future<bool> _getStoragePermission() async {
+    if (await Permission.storage.request().isGranted) {
+      return true;
+    } else if (await Permission.storage.request().isPermanentlyDenied) {
+      //todo: show dialog to choose this option
+      await openAppSettings();
+    } else if (await Permission.storage.request().isDenied) {
+      return false;
+    }
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +49,19 @@ class BackupCredentialPage extends StatelessWidget {
             context: context,
             textColor: Theme.of(context).colorScheme.onPrimary,
             onPressed: () async {
-              var directory = await getTemporaryDirectory();
-              var file = File('${directory.path}/my_files.txt');
-              print(directory.path);
-              await file.writeAsString('My name is Khan');
-              await OpenFile.open(file.path);
+              final downloadDirectory = await _getDownloadDirectory();
+              final isPermissionStatusGranted = await _getStoragePermission();
+
+              if (isPermissionStatusGranted) {
+                final savePath = path.join(downloadDirectory!.path);
+
+                final _myFile = File('$savePath/credential.txt');
+                await _myFile.writeAsString('My name is Khan');
+              } else {
+                // show snackbar after user declines
+              }
             },
-            child: Text("Backup - Change this text"),
+            child: Text('Backup'),
           )
         ],
       ),
