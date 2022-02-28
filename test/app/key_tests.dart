d19446edfa2b273fcec3e267e481fa10ac609160
Bibash Shrestha <bibashshrestha@gmail.com>
2022-02-28 13:46:24 +0545

    attempting to encrypt and dectypt

--- test/app/key_tests.dart
+++ test/app/key_tests.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:talao/app/interop/key_generation.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:crypto_keys/crypto_keys.dart';
 
 void main() {
   group('KeyGeneration', () {
@@ -13,6 +16,43 @@ void main() {
           generatedKey,
           equals(
               '{"kty":"OKP","crv":"Ed25519","d":"wHwSUdy4a00qTxAhnuOHeWpai4ERjdZGslaou-Lig5g=","x":"AI4pdGWalv3JXZcatmtBM8OfSIBCFC0o_RNzTg-mEAh6"}'));
+    });
+  });
+
+  group('CryptoKeys', () {
+    //todo: generate keypair using mnemonics
+    var keyPair = KeyPair.generateSymmetric(128);
+
+    var message = '{"name": "My name is Bibash Shresth"}';
+    var mnemonics =
+        'notice photo opera keen climb agent soft parrot best joke field devote';
+    var ivVector = 'Talao';
+
+    test('encryptAndDecrypt', () async {
+      //encryption
+      var encryptor =
+          keyPair.publicKey!.createEncrypter(algorithms.encryption.aes.gcm);
+      var encryptedBytes = encryptor.encrypt(
+        Uint8List.fromList(message.codeUnits),
+        additionalAuthenticatedData: Uint8List.fromList(mnemonics.codeUnits),
+        initializationVector: Uint8List.fromList(ivVector.codeUnits),
+      );
+
+      //decryption
+      var decrypter =
+          keyPair.privateKey!.createEncrypter(algorithms.encryption.aes.gcm);
+
+      var decryptedBytes = decrypter.decrypt(EncryptionResult(
+        encryptedBytes.data,
+        authenticationTag: encryptedBytes.authenticationTag,
+        additionalAuthenticatedData: Uint8List.fromList(mnemonics.codeUnits),
+        initializationVector: Uint8List.fromList(ivVector.codeUnits),
+      ));
+
+      var decryptedString = String.fromCharCodes(decryptedBytes);
+
+      print(decryptedString);
+      expect(decryptedString, equals(message));
     });
   });
 }
