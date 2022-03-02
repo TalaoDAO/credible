6f30ed814301bb41fd7a899d62537db4f0ab9886
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-02 21:09:21 +0545

    move blocprovider to specific page

--- lib/drawer/recovery_credential/view/recovery_credential_page.dart
+++ lib/drawer/recovery_credential/view/recovery_credential_page.dart
@@ -6,10 +6,15 @@ import 'package:talao/app/shared/widget/base/text_field.dart';
 import 'package:talao/drawer/recovery_credential/cubit/recovery_credential_cubit.dart';
 import 'package:talao/l10n/l10n.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:talao/wallet/cubit/wallet_cubit.dart';
 
 class RecoveryCredentialPage extends StatefulWidget {
   static Route route() => MaterialPageRoute(
-        builder: (context) => RecoveryCredentialPage(),
+        builder: (context) => BlocProvider(
+          create: (context) =>
+              RecoveryCredentialCubit(walletCubit: context.read<WalletCubit>()),
+          child: RecoveryCredentialPage(),
+        ),
         settings: RouteSettings(name: '/recoveryCredentialPage'),
       );
 
