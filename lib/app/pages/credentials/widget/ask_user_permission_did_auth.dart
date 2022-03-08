4d9232f4fb5518899752ddf826855aa69f937514
Bibash Shrestha <bibashshrestha@gmail.com>
2022-03-08 16:05:02 +0545

    attempting to change file location

--- lib/app/pages/credentials/widget/ask_user_permission_did_auth.dart
+++ lib/app/pages/credentials/widget/ask_user_permission_did_auth.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 import 'package:talao/app/pages/credentials/blocs/scan.dart';
-import 'package:talao/app/pages/credentials/pages/list.dart';
 import 'package:talao/app/shared/widget/confirm_dialog.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
