cd4b20f3a5c4e57b93db6b651d51825cdcd90f21
taleb <taleb.r75@gmail.com>
2022-03-23 00:05:48 +0430

    hide some information inside GeneralInformationPage for enterprise user

--- lib/drawer/global_information/widget/did_display.dart
+++ lib/drawer/global_information/widget/did_display.dart
@@ -7,6 +7,11 @@ import 'package:talao/did/did.dart';
 import 'package:talao/l10n/l10n.dart';
 
 class DIDDisplay extends StatelessWidget {
+  final bool isEnterpriseUser;
+
+  const DIDDisplay({Key? key, required this.isEnterpriseUser})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
@@ -31,59 +36,62 @@ class DIDDisplay extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(
-                    '${l10n.blockChainDisplayMethod} : ',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  Text(
-                    //TODO : Can we change did method name according to the user type?
-                    Constants.defaultDIDMethodName,
-                    style: Theme.of(context)
-                        .textTheme
-                        .bodyText2!
-                        .copyWith(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Text(
-                    '${l10n.blockChainAdress} : ',
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                  Expanded(
-                    child: Text(
-                      blockChainAddress != ''
-                          ? '${blockChainAddress.substring(0, 10)} ... ${blockChainAddress.substring(blockChainAddress.length - 10)}'
-                          : '',
-                      maxLines: 2,
+            if (!isEnterpriseUser)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      '${l10n.blockChainDisplayMethod} : ',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    Text(
+                      //TODO : Can we change did method name according to the user type?
+                      Constants.defaultDIDMethodName,
                       style: Theme.of(context)
                           .textTheme
                           .bodyText2!
                           .copyWith(fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.start,
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor:
-                    Theme.of(context).colorScheme.primary.withOpacity(0.1),
+            if (!isEnterpriseUser)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Text(
+                      '${l10n.blockChainAdress} : ',
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                    Expanded(
+                      child: Text(
+                        blockChainAddress != ''
+                            ? '${blockChainAddress.substring(0, 10)} ... ${blockChainAddress.substring(blockChainAddress.length - 10)}'
+                            : '',
+                        maxLines: 2,
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText2!
+                            .copyWith(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.start,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {
-                Clipboard.setData(ClipboardData(text: blockChainAddress));
-              },
-              child: Text(l10n.adressDisplayCopy),
-            ),
+            if (!isEnterpriseUser)
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor:
+                      Theme.of(context).colorScheme.primary.withOpacity(0.1),
+                ),
+                onPressed: () {
+                  Clipboard.setData(ClipboardData(text: blockChainAddress));
+                },
+                child: Text(l10n.adressDisplayCopy),
+              ),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
