721c987782c01cd915511d5843f04a115f017c15
taleb <taleb.r75@gmail.com>
2022-03-23 13:49:31 +0430

    fix minor things

--- lib/drawer/global_information/view/global_information_page.dart
+++ lib/drawer/global_information/view/global_information_page.dart
@@ -5,12 +5,12 @@ import 'package:talao/app/shared/widget/app_version.dart';
 import 'package:talao/app/shared/widget/back_leading_button.dart';
 import 'package:talao/app/shared/widget/base/page.dart';
 import 'package:talao/did/cubit/did_cubit.dart';
+import 'package:talao/drawer/global_information/cubit/global_information_cubit.dart';
+import 'package:talao/drawer/global_information/cubit/global_information_state.dart';
 import 'package:talao/drawer/global_information/widget/did_display.dart';
 import 'package:talao/drawer/global_information/widget/issuer_verification_setting.dart';
 import 'package:talao/l10n/l10n.dart';
 
-import '../cubit/global_information_cubit.dart';
-import '../cubit/global_information_state.dart';
 
 class GlobalInformationPage extends StatelessWidget {
   static Route route() => MaterialPageRoute(
