af2ccde6fe422d718ebb17e9f46459e661a2dc96
hawkbee1 <googandads@gmail.com>
2022-06-08 17:05:56 +0200

    Add missing delete during Wallet reset

--- lib/drawer/profile/cubit/profile_cubit.dart
+++ lib/drawer/profile/cubit/profile_cubit.dart
@@ -79,6 +79,8 @@ class ProfileCubit extends Cubit<ProfileState> {
     await secureStorageProvider.delete(SecureStorageKeys.jobTitle);
     await secureStorageProvider.delete(SecureStorageKeys.companyWebsite);
     await secureStorageProvider.delete(SecureStorageKeys.companyName);
+    await secureStorageProvider
+        .delete(SecureStorageKeys.issuerVerificationUrlKey);
     await secureStorageProvider.delete(SecureStorageKeys.isEnterpriseUser);
     await secureStorageProvider
         .delete(SecureStorageKeys.issuerVerificationUrlKey);
